d5f8a36f878ebd0a3151aaa9e1182ce470498e17
Galaxies <galaxies@vip.qq.com>
2020-05-04 20:10:53 -0700

    Update proj 1.

--- Project1/simple/simple.c
+++ Project1/simple/simple.c
@@ -1,14 +1,18 @@
+# include <linux/gcd.h>
+# include <linux/hash.h>
 # include <linux/init.h>
 # include <linux/kernel.h>
 # include <linux/module.h>
 
 int simple_init(void) {
   printk(KERN_INFO "Loading Kernel Module\n");
+  printk(KERN_INFO "The golden ratio prime is: %llu\n", GOLDEN_RATIO_PRIME);
   return 0;
 }
 
 void simple_exit(void) {
   printk(KERN_INFO "Removing Kernel Module\n");
+	printk(KERN_INFO "The greatest common divisor of %d and %d is: %lu\n", 3300, 24, gcd(3300, 24));
 }
 
 module_init(simple_init);
